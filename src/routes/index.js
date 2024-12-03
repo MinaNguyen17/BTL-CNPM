74db3c14263d041ae302f17dbaf818d50df8c13b
Nguyen Phuoc Ngoc Huong <155721693+MinaNguyen17@users.noreply.github.com>
2024-12-03 22:39:41 +0700

    add log in

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const fileRoute = require("./file");
 const printerRoute = require("./printer.js");
+const {
+  createAccount,
+  login,
+  authenticate,
+} = require("../controller/accountController");
 
 const router = express.Router();
 
-router.use("/file", fileRoute);
-router.use("/printer", printerRoute);
+router.use("/file", authenticate, fileRoute);
+router.use("/printer", authenticate, printerRoute);
+router.post("/register", createAccount);
+router.post("/login", login);
 
 module.exports = router;
