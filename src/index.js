974edb23401d7ecab335627496c670a071a34155
Nguyễn Lê Minh Huân <163861214+Kingatemis@users.noreply.github.com>
2024-12-05 20:18:46 +0700

    fixsever

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const mainRoutes = require("./routes/index");
 const connectDB = require("./config/db.js");
 const dotenv = require("dotenv");
+const cors = require("cors"); // import cors package
 
 // Load environment variables
 dotenv.config();
@@ -14,6 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
+
+// Cấu hình CORS
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',  // Chỉ cho phép frontend từ địa chỉ này
+  methods: 'GET,POST',  // Các phương thức được phép
+  allowedHeaders: 'Content-Type, Authorization'  // Các header được phép
+}));
 
 // Routes
 app.use("/", mainRoutes);
