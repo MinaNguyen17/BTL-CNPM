3db9f96a7f498d5bebf6b1c512c60355bf07b3b3
Nguyen Phuoc Ngoc Huong <155721693+MinaNguyen17@users.noreply.github.com>
2024-12-06 10:42:41 +0700

    Render view

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const mainRoutes = require("./routes/index");
 const connectDB = require("./config/db.js");
 const dotenv = require("dotenv");
@@ -11,12 +12,36 @@ connectDB();
 const app = express();
 
 // Middleware
+app.use("/static", express.static(path.join(__dirname, "../fronEnd/static")));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/", mainRoutes);
+// Route để render file HTML từ thư mục frontend
+
+app.get("/login/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/login/index.html"));
+});
+
+app.get("/login/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/login/login.html"));
+});
+
+app.get("/homePage/home2", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/home2.html"));
+});
+
+//trang chủ
+app.get("/homePage/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/index.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
