8f35876be190a30e38d3d199245f7d4be63b009f
Tanlocdev04 <loc.nguyendev04@hcmut.edu.vn>
2024-12-06 19:27:04 +0700

    config hay printPreview ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const mainRoutes = require("./routes/index");
 const connectDB = require("./config/db.js");
 const dotenv = require("dotenv");
-const cors = require("cors"); // import cors package
 
 // Load environment variables
 dotenv.config();
@@ -12,19 +12,45 @@ connectDB();
 const app = express();
 
 // Middleware
+app.use("/static", express.static(path.join(__dirname, "../fronEnd/static")));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 
-// Cấu hình CORS
-app.use(cors({
-  origin: '*', // Cho phép tất cả các domain
-  methods: 'GET,POST',  // Các phương thức được phép
-  allowedHeaders: 'Content-Type, Authorization'  // Các header được phép
-}));
-
 // Routes
 app.use("/", mainRoutes);
+// Route để render file HTML từ thư mục frontend
+
+app.get("/login/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/login/index.html"));
+});
+
+app.get("/login/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/login/login.html"));
+});
+
+app.get("/homePage/home2", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/home2.html"));
+});
+
+//trang chủ
+app.get("/homePage/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/index.html"));
+});
+
+//uploadfile
+app.get("/homePage/uploadfile", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/uploadfile.html"));
+});
+
+app.get("/homePage/config", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/config.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fronEnd/homePage/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
